223a4c6b57da77bb9292c0ec3434b3d954ecb8c2
Keegan Walker <127729270+K33gan123@users.noreply.github.com>
2025-10-19 15:03:57 +0200

    Add src/data/portfolioData.js

---
+++ src/data/portfolioData.js
@@ -0,0 +1,20 @@
+// This file will be dynamically generated with user data
+const portfolioData = {
+  header: {
+    name: "Keegan Walker",
+    title: "Professional",
+    summary: "Aspiring Software Engineer"
+  },
+  skills: ["Java","C++","Javascript","VSCode","Github","WSL","PHP","Bootstrap","React","PowerShell"],
+  experience: [{"title":"Intern","company":"Neozeit (File56) - Internship","startDate":"19/06/2025","endDate":"18/07/2025","extra":["Helped to design the architecture for a forensic dashboard, as well as develop a centralised React API and frontend to automate two complementary PowerShell Programs."]},{"title":"","company":"Crawford International Lonehill","startDate":"2021","endDate":"","extra":["Co-developed a client dashboard (PHP + Bootstrap) for products to be listed from the suppliers and exposed to one of their clients’ Doctor portals."]}],
+  education: [{"institution":"University of Pretoria, South Africa","degree":"BSc Information and Knowledge Systems","field":"Information and Knowledge Systems","endDate":"Expected Graduation: 11/2025","gpa":""}],
+  projects: [{"title":"","description":"Client dashboard (PHP + Bootstrap) for products to be listed from the suppliers and exposed to one of their clients’ Doctor portals.","technologies":["PHP","Bootstrap"],"dateRange":"","url":""},{"title":"","description":"PowerShell Program used to extract data from clients’ M365 or Entra Accounts and Mailboxes.","technologies":["PowerShell"],"dateRange":"","url":""},{"title":"","description":"PowerShell Program receives files generated from the Extractor to analyse and sometimes display data into tangible information.","technologies":["PowerShell"],"dateRange":"","url":""}],
+  contact: {
+    email: "",
+    phone: "",
+    linkedin: "",
+    github: ""
+  }
+};
+
+export default portfolioData;
